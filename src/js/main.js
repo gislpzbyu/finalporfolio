37a52b03db86d7dc8aa80fb605d9b744eef5da02
gislpzbyu <gislpzbyu@byui.edu>
2023-07-19 21:27:25 -0600

    video

--- src/js/main.js
+++ src/js/main.js
@@ -135,7 +135,7 @@ document.querySelector('#app').innerHTML = `
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Best Gifts <img id=logo src="images/giphy.jpg"/></a>
+            <a class="navbar-brand" href="#">Best Gifs <img id=logo src="images/giphy.jpg"/></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
